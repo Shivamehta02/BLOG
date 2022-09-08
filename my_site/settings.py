f20d96781d62551e572af310d2a3d80a7d44de77
Shiva Mehta <shivamehta73@gmail.com>
2022-09-08 12:35:46 +0530

    update

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import mimetypes
-from msilib.schema import AdminExecuteSequence
 import os
 from os import getenv
 from pathlib import Path
